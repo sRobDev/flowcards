0c65355303d9dcec1937a2d3162c9ed491c0105c
Samuel Robertson <sprobertson94@gmail.com>
2021-06-30 23:58:57 -0600

    Migration to MongoDB plus infra for .env files (#1)
    * Migration to MongoDB plus infra for .env files
    
    * Didn't actually need that long of a dev command...

--- server/app.js
+++ server/app.js
@@ -1,95 +1,100 @@
-const { PrismaClient } = require('@prisma/client');
+require('dotenv').config();
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
 const koaBody = require('koa-body');
 const router = require('@koa/router')();
 const cors = require('@koa/cors');
+const mongoose = require('mongoose');
+const Card = require('./models/card');
+const Topic = require('./models/topic');
+const User = require('./models/user');
+
 const app = module.exports = new Koa();
-const prisma = new PrismaClient();
+const DB_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@flowcards.1v2he.mongodb.net/flowdb?retryWrites=true&w=majority`;
 
 app.use(koaBody());
 app.use(cors());
 
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('[db] Connected to db, starting app...');
+    app.listen(3001);
+  })
+  .catch((err) => console.error(err));
+
 router.get('/', hello)
-  .post('/user/new', createUser)
-  .get('/user/:id', getUser)
-  .get('/cards/all/:id', getAllCards)
-  .post('/card/new/:id', createCard)
-  .post('/card/delete/:id', deleteCard)
-  .post('/card/:id/update', updateCard)
-  .get('/card/:id', getCard)
-  .post('/topic/new/:id', createTopic)
-  .post('/topic/delete/:id', deleteTopic)
-  .get('/topic/:id', getTopic);
+  .post('/v2/user/new', createUser)
+  .get('/v2/user/:id', getUser)
+
+  .post('/v2/card/new', createCard)
+  .get('/v2/cards/all/:authorId', getAllCards)
+  .post('/v2/card/delete/:id', deleteCard)
+  .put('/v2/card/:id/update', updateCard)
+  .get('/v2/card/:id', getCard)
+  
+  .post('/v2/topic/new/:authorId', createTopic)
+  .get('/v2/topics/all/:authorId', getAllTopics)
+  .get('/v2/topic/update/:id', updateTopic)
+  .delete('/v2/topic/delete/:id', deleteTopic)
+  .get('/v2/topic/:id', getTopic);
 
 app.use(router.routes());
 
+async function createCard(ctx) {
+  const card = new Card(ctx.request.body);
+
+  ctx.body = await card.save();
+}
 
 async function hello(ctx) {
-  ctx.body = 'Hello World';
+  ctx.body = 'Flowcards API is healthy and responding!';
 }
 
 async function getUser(ctx) {
-  ctx.body = await prisma.user.findUnique({ where: { id: parseInt(ctx.params.id) } })
+  ctx.body = await User.findOne({ _id: ctx.params.id });
 }
 
 async function createUser(ctx) {
-  ctx.body = await prisma.user.create({
-    data: {
-      name: ctx.request.body.name
-    }
-  });
+  const user = new User(ctx.request.body);
+  ctx.body = await user.save();
 }
 
 async function getAllCards(ctx) {
-  ctx.body = await prisma.card.findMany({where: { authorId: +ctx.params.id }});
+  ctx.body = await Card.find({ authorId: ctx.params.authorId });
 }
 
 async function getCard(ctx) {
-  ctx.body = await prisma.card.findUnique({ where: { id: parseInt(ctx.params.id) } })
-}
-
-async function createCard(ctx) {
-  const authorId = +ctx.params.id;
-
-  ctx.body = await prisma.card.create({ data: {...ctx.request.body, authorId } });
+  ctx.body = await Card.findOne({ _id: ctx.params.id });
 }
 
 async function deleteCard(ctx) {
-  ctx.body = await prisma.card.delete({ where: { id: +ctx.params.id } });
+  ctx.body = await Card.deleteOne({ _id: ctx.params.id });
 }
 
 async function updateCard(ctx) {
-  ctx.request.body.topicId = +ctx.request.body.topicId || null;
-  ctx.body = await prisma.card.update({
-    where: {
-      id: parseInt(ctx.params.id)
-    },
-    data: { ...ctx.request.body }
-  });
+  ctx.body = Card.updateOne({ _id: ctx.params.id }, ctx.request.body);
 }
 
 async function getTopic(ctx) {
-  ctx.body = await prisma.topic.findUnique({
-    where: {
-      id: parseInt(ctx.params.id)
-    },
-    include: {
-      cards: true
-    }
-  });
+  ctx.body = await Card.findOne({ _id: ctx.params.id });
+}
+
+async function getAllTopics(ctx) {
+  ctx.body = await Topic.find({ authorId: ctx.params.authorId });
 }
 
 async function createTopic(ctx) {
-  const authorId = +ctx.params.id;
+  const topic = new Topic({
+    ...ctx.request.body,
+    authorId: ctx.params.authorId
+  });
 
-  ctx.body = await prisma.topic.create({ data: {...ctx.request.body, authorId } });
+  ctx.body = await topic.save();
+}
+
+async function updateTopic(ctx) {
+  ctx.body = Topic.updateOne({ _id: ctx.params.id }, ctx.request.body);
 }
 
 async function deleteTopic(ctx) {
-  ctx.body = await prisma.topic.delete({ where: { id: +ctx.params.id } });
+  ctx.body = await Topic.deleteOne({ _id: ctx.params.id });
 }
-
-const simpleCreate = async (body, table) => await prisma[table].create({ data: {...body } });
-
-if (!module.parent) app.listen(3001);
